032113b1f0e63798ca09729ff08b2b2b0952c284
OtsokovShA <shamil24@mail.ru>
2021-11-01 08:23:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/toolshop/Drill.java
@@ -0,0 +1,45 @@
+package com.example.toolshop;
+
+public class Drill {
+    private String title;
+    private String info;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+
+    private int imageResourceId;
+
+
+    public Drill(String title, String info, int imageResourceId) {
+        this.title = title;
+        this.info = info;
+        this.imageResourceId = imageResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+}
